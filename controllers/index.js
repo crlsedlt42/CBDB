19ae4d99602b929eac88c3d7bac2a9e2c2164a88
lafrefre <134004859+lafrefre@users.noreply.github.com>
2023-09-21 19:28:46 -0700

    "added index base and api base"

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,28 @@
-// Initialize router and set up routes
-const router = require('express').Router();
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const sequelize = require('./config/database'); // Import sequelize
+const User = require('./models/user'); // Import user model
+const apiRoutes =  require('./routes/api'); // Import all of the API routes
 
-// Import all of the API routes from /api/index.js
-const homeRoutes = require('./homeRoutes');
+const app = express();
+const PORT = process.env.PORT || 3000; // Default PORT
 
 
-// '/' is the endpoint and homeRoutes is the router object
-router.use('/', homeRoutes);
 
-// Exporting router object
-module.exports = router;
+
+
+// // Initialize router and set up routes
+// const router = require('express').Router();
+
+// // Import all of the API routes from /api/index.js
+// const homeRoutes = require('./homeRoutes');
+
+
+// // '/' is the endpoint and homeRoutes is the router object
+// router.use('/', homeRoutes);
+
+// // Exporting router object
+// module.exports = router;
---
+++ controllers/routes/api.js
@@ -0,0 +1,11 @@
+const express = require('express'); // Import express
+const router = express.Router(); // Initialize router
+const movie = require('../models/movie'); // Import movie model
+
+// Import all of the API routes from /api/index.js
+
+router.get('/movies', (req, res) => {
+    
+    
+})
+
