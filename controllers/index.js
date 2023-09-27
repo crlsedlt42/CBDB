d548fed605e1ec68f142accb2330e206b87060a4
crlse42 <carlosdlt42@hotmail.com>
2023-09-26 21:30:01 -0700

    Modified syntax errors for Logout button

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,11 @@ const router = require('express').Router();
 
 // Import all of the API routes from /api/index.js
 const homeRoutes = require('./homeRoutes');
-
+const apiRoutes = require('./api'); 
 
 // '/' is the endpoint and homeRoutes is the router object
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 // Exporting router object
 module.exports = router;
