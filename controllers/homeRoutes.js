1feab93959a0bb148dff5c5b0159f19b80b2b06a
crlse42 <carlosdlt42@hotmail.com>
2023-09-28 21:38:01 -0700

    Added code to homeRoutes and edited homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,36 @@
 const router = require('express').Router();
+const { Review, Movie } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const reviewData = await Movie.findAll({
+            include: [
+                // {
+                //     model: User,
+                //     attributes: ['name'],
+                // },
+                {
+                    model: Review,
+                    attributes: ['review_txt'],
+                },
+            ],
+        });
+
+
+    const reviews = reviewData.map((review) => review.get({ plain: true }));
+
+    console.log(reviews);
+
+    res.render('homepage', {
+        reviews,
+        logged_in: req.session.logged_in
+    });
+} catch (err) {
+    res.status(500).json(err);
+}
+});
+
+
 
 router.get('/', async (req, res) => {
     res.render('homepage', { 
